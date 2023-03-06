b618af1eff9ba040eed4e8df04f32f5619bacac7
Mariane <marianeh@bempromotora.com.br>
2023-03-06 18:45:50 -0300

    adiciona textField para colocar nome e cria regra caso nome vazio responder cm erro

--- src/view/login/login.tsx
+++ src/view/login/login.tsx
@@ -1,22 +1,39 @@
-import { Button } from "@mui/material";
-import React from "react";
+import { Button, TextField } from "@mui/material";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [invalidName, setInvalidName] = useState(false);
+  const [name, setName] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
-  }
+  };
 
-  const navigate = useNavigate();
-  
   function handleButtonClick() {
-    navigate('/home');
-
-  }
+    if (name === "") {
+      setInvalidName(true)
+    } else {
+      navigate("/home");
+    }
+  };
   return (
     <div>
       <form className="form" onSubmit={handleSubmit}>
-        <Button type="submit" onClick={handleButtonClick} > Entrar </Button>
+        <TextField
+          value={name}
+          id="standard-basic"
+          label="Seu nome aqui"
+          variant="standard"
+          error={invalidName}
+          onChange={(e) => setName(e.target.value)}
+          helperText={invalidName? 'Por favor, insira o seu nome' : ''}
+        />
+        <Button type="submit" onClick={handleButtonClick}>
+          Entrar
+        </Button>
       </form>
     </div>
   );
