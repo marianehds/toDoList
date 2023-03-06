579affce35fa5611b345f3a68245e35819bbade5
Mariane <marianeh@bempromotora.com.br>
2023-03-06 19:33:15 -0300

    cria scss do login, instala biblioteca scss

--- src/view/login/login.tsx
+++ src/view/login/login.tsx
@@ -1,6 +1,8 @@
-import { Button, TextField } from "@mui/material";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+import { Button, TextField } from "@mui/material";
+import "./Login.scss";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -10,32 +12,32 @@ const Login = () => {
 
   function handleSubmit(event) {
     event.preventDefault();
-  };
+  }
 
   function handleButtonClick() {
     if (name === "") {
-      setInvalidName(true)
+      setInvalidName(true);
     } else {
       navigate("/home");
     }
-  };
+  }
   return (
-    <div>
-      <form className="form" onSubmit={handleSubmit}>
-        <TextField
-          value={name}
-          id="standard-basic"
-          label="Seu nome aqui"
-          variant="standard"
-          error={invalidName}
-          onChange={(e) => setName(e.target.value)}
-          helperText={invalidName? 'Por favor, insira o seu nome' : ''}
-        />
-        <Button type="submit" onClick={handleButtonClick}>
-          Entrar
-        </Button>
-      </form>
-    </div>
+    <form className="form" onSubmit={handleSubmit}>
+      <TextField
+        value={name}
+        id="standard-basic"
+        label="Seu nome aqui"
+        variant="standard"
+        error={invalidName}
+        onChange={((e) => (setName(e.target.value), setInvalidName(false)))}
+        helperText={invalidName ? "Por favor, insira o seu nome" : ""}
+        size="medium"
+        color="secondary"
+      />
+      <Button type="submit" onClick={handleButtonClick} color="secondary" size="large">
+        Entrar
+      </Button>
+    </form>
   );
 };
 
