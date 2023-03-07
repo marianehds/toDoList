0dbdaf3c795e29378c44fcc452f3fc0d3af82220
Mariane <marianeh@bempromotora.com.br>
2023-03-07 20:28:14 -0300

    ajusta profile

---
+++ src/view/profile/Profile.tsx
@@ -0,0 +1,17 @@
+import { BigHead } from "@bigheads/core";
+import { Button, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Avatar from "./Avatar.tsx";
+
+const Profile = () => {
+   ;
+
+  return (
+    <div>
+      {Avatar}
+    </div>
+  );
+};
+
+export default Profile;
