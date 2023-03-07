34c700ae0a525178d2d52d9502123a1b259f96da
Mariane <marianeh@bempromotora.com.br>
2023-03-07 01:04:50 -0300

    ajusta estilo da home, ajusta nome de arquivo

--- src/view/home/home.tsx
+++ src/view/home/home.tsx
@@ -1,12 +1,154 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Icon,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import { Alarm, ExpandMore } from "@mui/icons-material";
 import React from "react";
+import Button from "@mui/material/Button";
+import "./Home.scss";
+import month from "../../assets/img/month.png";
 
 const Home = () => {
-
-
   return (
-    <div>
-     <p> oi, sou a home</p>
-    </div>
+    <>
+      <div className="header">
+        <img className="header--month" src={month} alt="escrita Month" />
+        <div className="header--user">
+          <p className="header--user-name">name lastname</p>
+          <Alarm fontSize="large" className="header--user-icon" />
+        </div>
+      </div>
+      <div className="week">
+        <div className="week--day">
+          <Accordion className="week--day-monday">
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Segunda</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div>
+                <Button> aa </Button>
+              </div>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+        <div className="week--day">
+          <Accordion className="week--day-tuesday">
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Terça</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+        <div className="week--day">
+          <Accordion className="week--day-wednesday">
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Quarta</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+        <div className="week--day">
+          <Accordion className="week--day-thursday">
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Quinta</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+        <div className="week--day">
+          <Accordion className="week--day-friday">
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Sexta</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+        <div className="week--day">
+          <Accordion className="week--day-saturday">
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Sabado</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+        <div className="week--day">
+          <Accordion className="week--day-sunday">
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Domingo</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+      </div>
+    </>
   );
 };
 
