dffc43ec7f71e93b3bd59be53fd6b9e7910fd0cb
Mariane <marianehds@outlook.com>
2023-10-17 18:00:34 -0300

    configura eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "consistent-return": "warn",
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "no-unused-vars": "warn",
+    "no-unused-expressions": "warn",
+    "no-restricted-properties": "warn",
+    "no-restricted-globals": "warn",
+    "no-param-reassign": "warn",
+  },
 };
