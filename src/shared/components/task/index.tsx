bbeae6fa360550d44b9ae647a3503c729b6303a2
Mari <marianehds@outlook.com>
2023-10-19 16:51:56 -0300

    insere checkbox estilizado no componente task

--- src/shared/components/task/index.tsx
+++ src/shared/components/task/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 
-import { Paper, Typography } from "@mui/material";
+import { Checkbox, Paper, Typography } from "@mui/material";
 import "./task.scss";
-
 
 type TTask = {
   key: number;
@@ -14,11 +13,14 @@ export const Task = ({ key, title, description }: TTask) => {
   return (
     <section data-page="paper-task">
       <Paper className="task-card" variant="outlined" key={key}>
-        <Typography fontSize={20} fontWeight={500}>
-          
-          {title}
-        </Typography>
-        <Typography fontSize={14}> {description}</Typography>
+        <div>
+          <Typography fontSize={20} fontWeight={500}>
+            {title}
+          </Typography>
+
+          <Typography fontSize={14}> {description}</Typography>
+        </div>
+        <Checkbox className="checkbox-task" />
       </Paper>
     </section>
   );
