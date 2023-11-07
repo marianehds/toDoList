2429308650b0fac65bc563b9335709b3207b4d1a
Mari <marianehds@outlook.com>
2023-11-07 19:57:17 -0300

    cria map para hair do avatar

--- src/view/profile/profile.tsx
+++ src/view/profile/profile.tsx
@@ -4,12 +4,24 @@ import "./profile.scss";
 import { Button, FormControlLabel, Radio, RadioGroup } from "@mui/material";
 import TAvatar from "../../shared/avatar/avatar.type";
 import { useNavigate } from "react-router-dom";
+import { AvatarProps, hairMap } from "@bigheads/core";
 
 const Profile = () => {
   const navigate = useNavigate();
   const userLocalStorage = localStorage.getItem("user")?.replace(/[\\"]/g, "");
 
   const [avatar, setAvatar] = useState<TAvatar>({});
+
+  const hairMapKeys = Object.keys(hairMap);
+  // const eyesMapKeys = Object.keys(eyesMap);
+  // const eyebrowsMapKeys = Object.keys(eyebrowsMap);
+  // const hairMapKeys = Object.keys(mouthsMap);
+  // const hairMapKeys = Object.keys(facialHairMap);
+  // const hairMapKeys = Object.keys(clothingMap);
+  // const hairMapKeys = Object.keys(accessoryMap);
+  // const hairMapKeys = Object.keys(hatMap);
+  // const hairMapKeys = Object.keys(mouthsMap);
+  // const hairMapKeys = Object.keys(mouthsMap);
 
   return (
     <section data-page="profile">
@@ -25,27 +37,20 @@ const Profile = () => {
         name="radio-group-hair"
         style={{ display: "inline" }}
       >
-        <FormControlLabel
-          value="none"
-          control={<Radio />}
-          label="None"
-          onClick={() => setAvatar({ ...avatar, hair: "none" })}
-        />
-        <FormControlLabel
-          value="long"
-          control={<Radio />}
-          label="Long"
-          onClick={() => setAvatar({ ...avatar, hair: "long" })}
-        />
-        <FormControlLabel
-          value="short"
-          control={<Radio />}
-          label="Short"
-          onClick={() => setAvatar({ ...avatar, hair: "short" })}
-        />
+        {hairMapKeys.map((item) => (
+          <FormControlLabel
+            key={item}
+            label={item}
+            value={item}
+            control={<Radio />}
+            onClick={() =>
+              setAvatar({ ...avatar, hair: item as AvatarProps["hair"] })
+            }
+          />
+        ))}
       </RadioGroup>
 
-      <Button onClick={() =>  navigate("/home")}> Voltar </Button>
+      <Button onClick={() => navigate("/home")}> Voltar </Button>
     </section>
   );
 };
