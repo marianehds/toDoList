9f5a21ffe3dc8aaa83548d46a3c867616c8eecfc
Mari <marianehds@outlook.com>
2023-11-07 19:08:31 -0300

    adiciona data page em todas as pagina e adiciona index para view

---
+++ src/view/index.ts
@@ -0,0 +1,6 @@
+
+export { default as Login } from "./login/login";
+export { default as Home } from "./home/home";
+export { default as Profile } from "./profile/profile";
+
+import "./index.scss";
